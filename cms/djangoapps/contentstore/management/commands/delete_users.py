1f83594d28b2eaa1347ef953e828f4eefd9c22d8
mcigna <marco.cigna@gses.com.au>
2022-08-02 07:08:37 +0000

    Added funtionality to delete specific users via --only flag

--- cms/djangoapps/contentstore/management/commands/delete_users.py
+++ cms/djangoapps/contentstore/management/commands/delete_users.py
@@ -6,7 +6,6 @@ from django.contrib.auth.models import User
 from cms.djangoapps.course_creators.models import CourseCreator
 from django.core.exceptions import ObjectDoesNotExist
 
-
 class Command(BaseCommand):
     """
     Delete users in bulk, with option to exclude specifc users
@@ -15,6 +14,7 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('--exclude', nargs='+', type=str, help="Add usernames of users to be excluded from deletion")
+        parser.add_argument('--only', nargs='+', type=str, help="Add usernames of users to be deleted")
 
     def handle(self, *args, **options):
         def delete_users (users):
@@ -24,7 +24,7 @@ class Command(BaseCommand):
             else:
                 print("Found {} users to delete.".format(len(users)))
                 for user in users:
-                    course_creator =CourseCreator.objects.filter(user_id=user.id).first()
+                    course_creator = CourseCreator.objects.filter(user_id=user.id).first()
                     print("Deleting user: {}".format(user))
                     if course_creator: course_creator.delete()
                     user.delete()
@@ -46,8 +46,14 @@ class Command(BaseCommand):
             print('=' * 30)
             print("Following users are being excluded")
             print(exclude_users)
+            print('=' * 30)
+            print("Deleting Users")
+            all_users=User.objects.all().exclude(username__in=users_to_exclude)
+            delete_users(all_users)
 
-        print('=' * 30)
-        print("Deleting Users")
-        all_users=User.objects.all().exclude(username__in=users_to_exclude)
-        delete_users(all_users)
+        elif options['only']:
+            selected_users = options['only']
+            filtered_users = User.objects.filter(username__in=selected_users)
+            print('=' * 30)
+            print("Deleting Users")
+            delete_users(filtered_users)
